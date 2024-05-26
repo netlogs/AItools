4aaf1cc1200060df97f9d97f4b4038e8a33c74ad
netlogs <2406292852@qq.com>
2024-05-26 16:40:14 +0800

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import robotsTxt from "astro-robots-txt";
-
 import vercel from "@astrojs/vercel/serverless";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.DEV ? "http://localhost:4321" : "https://luna-landing-rust.vercel.app/",
   integrations: [tailwind(), sitemap(), robotsTxt()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+	webAnalytics: {enabled: true}
+  }),
 });
